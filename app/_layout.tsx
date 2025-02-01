20f7bb88791f566fcea975b789dabd525f7d6a5d
Patric Gekoski <=>
2025-02-01 17:05:12 -0500

    Add auth logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,25 @@
+import { AuthProvider } from '@/context/AuthContext'
 import { COLORS } from '@/utils/colors'
 import { Stack } from 'expo-router'
 
 export default function RootLayout() {
 	return (
-		<Stack
-			screenOptions={{
-				headerStyle: {
-					backgroundColor: COLORS.background,
-				},
-				headerTintColor: '#FFF',
-				contentStyle: {
-					backgroundColor: COLORS.background,
-				},
-			}}
-		>
-			<Stack.Screen name='index' options={{ headerShown: false }} />
-			<Stack.Screen name='privacy' options={{ presentation: 'modal', title: 'Privacy Policy' }} />
-			<Stack.Screen name='register' options={{ title: 'Create Account', headerBackTitle: 'Login' }} />
-		</Stack>
+		<AuthProvider>
+			<Stack
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: COLORS.background,
+					},
+					headerTintColor: '#FFF',
+					contentStyle: {
+						backgroundColor: COLORS.background,
+					},
+				}}
+			>
+				<Stack.Screen name='index' options={{ headerShown: false }} />
+				<Stack.Screen name='privacy' options={{ presentation: 'modal', title: 'Privacy Policy' }} />
+				<Stack.Screen name='register' options={{ title: 'Create Account', headerBackTitle: 'Login' }} />
+			</Stack>
+		</AuthProvider>
 	)
 }
